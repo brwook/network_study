d7bc0a7ec0642d1366e1cf9f9a3e944b584b06d0
brwook <dladnguq131@naver.com>
2022-03-27 10:18:39 -0400

    [*] assignment1: add-nbo

--- add-nbo/main.c
+++ add-nbo/main.c
@@ -4,18 +4,17 @@
 
 uint32_t openConvert(const char *path)
 {
-	uint32_t res;
-	FILE *fp = fopen(path, "r");
-	if(fp == NULL)
-	{
-		fprintf(stderr, "file open error\n");
-		return 0;
-	}
-
-	fread(&res, 4, 1, fp);
-	res = ntohl(res);
-	fclose(fp);
-	return res;
+    uint32_t res;
+    FILE *fp = fopen(path, "r");
+    if(fp == NULL)
+    {
+        fprintf(stderr, "file open error\n");
+        return 0;
+    }
+    fread(&res, 4, 1, fp);
+    res = ntohl(res);
+    fclose(fp);
+    return res;
 }
 
 int main(int argc, char *argv[])
@@ -24,6 +23,6 @@ int main(int argc, char *argv[])
     uint32_t val_2 = openConvert(argv[2]);
     uint32_t sum = val_1 + val_2;
     printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", val_1, val_1, val_2, val_2, sum, sum);
-	return 0;
+    return 0;
 }
 
